0f6301e30527c5bfd87236278d8697a01db9dc0d
Cris <cristian.correa.cs@gmail.com>
2026-09-21 16:38:53 -0500

    Configure Prettier, WGSL loaders and WebGPU types

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const wgslLoader = "@vgpu/wgsl/loader-webpack";
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    rules: {
+      "*.wgsl": { loaders: [wgslLoader], as: "*.js" },
+    },
+  },
+  webpack(config) {
+    config.module.rules.push({ test: /\.wgsl$/, loader: wgslLoader });
+    return config;
+  },
 };
 
 export default nextConfig;
